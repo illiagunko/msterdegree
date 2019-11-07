0552c9adea57596930983c8a04625bf459dfdcca
illiagunko <40844577+illiagunko@users.noreply.github.com>
2019-11-07 14:26:38 +0200

    Update scaleengine.sh

--- scaleengine.sh
+++ scaleengine.sh
@@ -4,21 +4,20 @@ cpu_quantity = cat /proc/cpuinfo | grep processor | wc -l
 memory_amount = free -m | awk '{print $2}' | awk ‘(NR == 2)’
 current_memory = free -m | awk '{print $4}’ | awk ‘(NR == 2)’
 let “memory_usage_percent = current_memory/memory_amount*100”
+
+cut -c-3 /proc/loadavg > la.txt
+current_la = cat la.txt
+printf "%.0f\n" $current_la > la.txt
+current_la = cat la.txt
+
 counter=0
+
 limit_up = cat servicescalepolicy | grep max | awk -F=" " '{print $2}'
 limit_down = cat servicescalepolicy | grep min | awk -F=" " '{print $2}'
 current_pods = cat Deployment | grep replicas | awk -Freplicas:" " '{print $2}'
+
 rm Deployment.template
 wget https://github.com/illiagunko/msterdegree/blob/master/configuration/Deployment.template
-
-#if counter; else ; fi
-if [ "$counter" -gt 0 ]; 
-  then let "new_pods = current_pods + 1"
-    sed 's/{pods}/$new_pods' ./Deployment
-elif [ "$counter" -lt 0 ];
-  then let "new_pods = current_pods - 1"
-    sed 's/{pods}/$new_pods' ./Deployment
-fi
 
 if [ "$counter" -gt 0 ]; 
     then let "new_pods = current_pods + 1"
